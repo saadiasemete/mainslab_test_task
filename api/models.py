a96aae8a4beae736697869d483fced5a952432db
Solti Amosa Mita <sortia@protonmail.com>
2021-11-01 23:40:08 +0300

    message isn't stored

--- api/models.py
+++ api/models.py
@@ -6,8 +6,6 @@ class SentMessageDataQuerySet(models.QuerySet):
         oldest = dtm.now()-tmd(days=1)
         return self.filter(created_at__gte = oldest)
 
-
-
 class SentMessageDataManager(models.Manager):
     def get_queryset(self):
         return SentMessageDataQuerySet(self.model, using=self._db)
@@ -15,11 +13,9 @@ class SentMessageDataManager(models.Manager):
     def last_24_hours(self):
         return self.get_queryset().last_24_hours()
 
-
 class SentMessageData(models.Model):
     id = models.UUIDField(primary_key=True)
     subject = models.CharField(max_length=256)
-    message = models.TextField()
     to_email = models.EmailField()
     created_at = models.DateTimeField(auto_now_add=True)
 
